87ffd1df835aa51682576203b412f5c60d33db60
BittingBarracuda <kmartinez2397@gmail.com>
2023-01-04 21:39:36 +0100

    Added function declarations to matrix_ops.h

---
+++ matrix/matrix_ops.h
@@ -0,0 +1,16 @@
+#include "matrix.h"
+
+int check_dimensions(matrix* m1, matrix* m2);
+int can_dot(matrix* m1, matrix* m2);
+double sum(matrix* m);
+double prod(matrix* m);
+matrix* abs_mat(matrix* m);
+matrix* add(matrix* m1, matrix* m2);
+matrix* sub(matrix* m1, matrix* m2);
+matrix* mult(matrix* m1 , matrix* m2);
+matrix* div(matrix* m1, matrix* m2);
+matrix* power(matrix* m1, int p);
+matrix* apply(matrix* m1, double (*f)(double));
+matrix* dot(matrix* m1, matrix* m2);
+matrix* scale(matrix* m1, double c);
+matrix* scalar_sum(matrix* m1, double c);
