49a0ac80fedcd928a1c484827500439ed94a5acf
BittingBarracuda <kmartinez2397@gmail.com>
2023-01-07 20:38:14 +0100

    Implemented a few operations for matrix_vector_ops

---
+++ matrix_vector_ops.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include "matrix_vector_ops.h"
+
+int check_dimensions_rows(matrix* m, vector* v) {
+    if(m->rows == v->dim) return 1;
+    return 0;
+}
+
+int check_dimensions_cols(matrix* m, vector* v) {
+    if(m->cols == v->dim) return 1;
+    return 0;
+}
+
+matrix* add_rows(matrix* m, vector* v) {
+    if(check_dimensions_cols(m, v)) {
+        matrix* n = new_matrix(m->rows, m->cols);
+        for(unsigned int i = 0; i < m->rows; i++) {
+            for(unsigned int j = 0; j < m->cols; j++) {
+                n->mat[i][j] = v->vec[j] + m->mat[i][j];
+            }
+        }
+        return n;
+    } else {
+        exit(1);
+    }
+}
+
+matrix* add_cols(matrix* m, vector* v) {
+    if(check_dimensions_rows(m, v)) {
+        matrix* n = new_matrix(m->rows, m->cols);
+        for(unsigned int i = 0; i < m->cols; i++) {
+            for(unsigned int j = 0; j < m->rows; j++) {
+                n->mat[j][i] = v->vec[j] + m->mat[j][i];
+            }
+        }
+        return n;
+    } else {
+        exit(1);
+    }
+}
+
+
+matrix* sub_rows(matrix* m, vector* v) {
+    if(check_dimensions_cols(m, v)) {
+        matrix* n = new_matrix(m->rows, m->cols);
+        for(unsigned int i = 0; i < m->rows; i++) {
+            for(unsigned int j = 0; j < m->cols; j++) {
+                n->mat[i][j] = v->vec[j] - m->mat[i][j];
+            }
+        }
+        return n;
+    } else {
+        exit(1);
+    }
+}
+
+matrix* sub_cols(matrix* m, vector* v) {
+    if(check_dimensions_rows(m, v)) {
+        matrix* n = new_matrix(m->rows, m->cols);
+        for(unsigned int i = 0; i < m->rows; i++) {
+            for(unsigned int j = 0; j < m->cols; j++) {
+                n->mat[i][j] = v->vec[j] - m->mat[i][j];
+            }
+        }
+        return n;
+    } else {
+        exit(1);
+    }
+}
+
+
+matrix* dot_mv(matrix* m, vector* v);
+matrix* dot_vm(vector* v, matrix* m);
--- matrix_vector_ops.h
+++ matrix_vector_ops.h
@@ -1,7 +1,11 @@
 #include "vector_ops.h"
 #include "matrix_ops.h"
 
-int check_dimensions(matrix* m, vector* v);
-matrix* add(matrix* m, vector* v);
-matrix* sub(matrix* m, vector* v);
-matrix* dot(matrix* m, vector* v);
+int check_dimensions_rows(matrix* m, vector* v);
+int check_dimensions_cols(matrix* m, vector* v);
+matrix* add_rows(matrix* m, vector* v);
+matrix* add_cols(matrix* m, vector* v);
+matrix* sub_rows(matrix* m, vector* v);
+matrix* sub_cols(matrix* m, vector* v);
+matrix* dot_mv(matrix* m, vector* v);
+matrix* dot_vm(vector* v, matrix* m);
