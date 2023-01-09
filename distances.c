6c25e656af5c2318302672eb405e004d2786a442
BittingBarracuda <kmartinez2397@gmail.com>
2023-01-09 13:44:18 +0100

    Fixed memory leaks with inplace operations.

--- distances.c
+++ distances.c
@@ -4,20 +4,20 @@
 #include "distances.h"
 
 vector* euclidean(matrix* m, vector* x) {
-    matrix* tmp = sub_rows(m, x);
-    tmp = power(tmp, 2);
-    tmp = sum_rows(tmp);
-    tmp = apply(tmp, sqrt);
-    vector* vec = vector_from_matrix(tmp);
-    free_matrix(tmp);
+    matrix* tmp1 = sub_rows(m, x);
+    power_inplace(tmp1, 2);
+    matrix* tmp2 = sum_rows(tmp1);
+    apply_inplace(tmp2, sqrt);
+    vector* vec = vector_from_matrix(tmp2);
+    free_matrix(tmp1); free_matrix(tmp2);
     return vec;
 }
 
 vector* manhattan(matrix* m, vector* x) {
-    matrix* tmp = sub_rows(m, x);
-    tmp = abs_mat(tmp);
-    tmp = sum_rows(tmp);
-    vector* vec = vector_from_matrix(tmp);
-    free_matrix(tmp);
+    matrix* tmp1 = sub_rows(m, x);
+    abs_mat_inplace(tmp1);
+    matrix* tmp2 = sum_rows(tmp1);
+    vector* vec = vector_from_matrix(tmp2);
+    free_matrix(tmp1); free_matrix(tmp2);
     return vec;
 }
