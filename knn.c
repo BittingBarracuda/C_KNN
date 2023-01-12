a13c7bb2d655a3031129e1e19bb23e66800f89e1
BittingBarracuda <kmartinez2397@gmail.com>
2023-01-12 10:54:37 +0100

    Changed splitting of data and classes.

--- knn.c
+++ knn.c
@@ -8,7 +8,7 @@ int* fit(matrix* train, matrix* test, int* classes, vector* (*dist)(matrix*, vec
 int* read_classes_file(char* file_path, int n_instances);
 unsigned int* get_nearest_k(matrix* data, vector* x, vector* (*dist)(matrix*, vector*), unsigned int k);
 int get_class(int* classes, unsigned int* closest, int k);
-void split_classes_array(int* classes, int* classes_train, int* classes_test, double training_split);
+void split_classes_array(int* classes, int* classes_train, int* classes_test, unsigned int n1, unsigned int n2);
 double get_precission(int* classes, int* predicted_classes);
 
 int* fit(matrix* train, matrix* test, int* classes, vector* (*dist)(matrix*, vector*), unsigned int k) {
@@ -23,6 +23,7 @@ int* fit(matrix* train, matrix* test, int* classes, vector* (*dist)(matrix*, vec
 }
 
 int* read_classes_file(char* file_path, int n_instances) {
+    printf("llego");
     FILE* fd = fopen(file_path, "r");
     char line[MAXINPUT];
     int* classes = calloc(n_instances, sizeof(int));
@@ -67,18 +68,14 @@ int get_class(int* classes, unsigned int* closest, int k) {
     return max;
 }
 
-void split_classes_array(int* classes, int* classes_train, int* classes_test, double training_split) {
-    if(training_split >= 0.0 && training_split <= 1.0) {
-        int size = sizeof(classes) / sizeof(int);
-        int n_rows_train = (int)(0.7 * size);
-        int n_rows_test = size - n_rows_train;
-        classes_train = calloc(n_rows_train, sizeof(int));
-        classes_test = calloc(n_rows_test, sizeof(int));
-        for(unsigned int i = 0; i < n_rows_train; i++) {
+void split_classes_array(int* classes, int* classes_train, int* classes_test, unsigned int n1, unsigned int n2) {
+    int size = sizeof(classes) / sizeof(int);
+    if(n1 < size && n2 < size && n1 + n2 == size) {
+        for(unsigned int i = 0; i < n1; i++) {
             *(classes_train + i) = *(classes + i);
         }
-        for(unsigned int i = 0; i < n_rows_test; i++) {
-            *(classes_test + i) = *(classes + n_rows_train + i);
+        for(unsigned int i = 0; i < n2; i++) {
+            *(classes_test + i) = *(classes + n1+ i);
         }
     }
 }
@@ -107,10 +104,15 @@ int main(void) {
     int* classes = read_classes_file(classes_path, data->rows);
     shuffle_with_classes(data, classes);
     printf("[!] Datasets files read and saved!\n");
-    matrix* train; matrix* test; int* classes_train; int* classes_test;
     printf("[!] Splitting dataset into test and train sets...\n");
-    get_matrix_split(data, train, test, 0.7);
-    split_classes_array(classes, classes_train, classes_test, 0.7);
+    unsigned int n_rows_train = (unsigned int)(0.7 * data->rows);
+    unsigned int n_rows_test = data->rows - n_rows_train;
+    matrix* train = new_matrix(n_rows_train, data->cols);
+    matrix* test = new_matrix(n_rows_test, data->cols);
+    int* classes_train = calloc(n_rows_train, sizeof(int));
+    int* classes_test = calloc(n_rows_test, sizeof(int));
+    get_matrix_split(data, train, test, n_rows_train, n_rows_test);
+    split_classes_array(classes, classes_train, classes_test, n_rows_test, n_rows_train);
     free_matrix(data); free(classes);
     printf("[!] Fitting test dataset...\n");
     int* predicted_classes = fit(train, test, classes_test, euclidean, k);
